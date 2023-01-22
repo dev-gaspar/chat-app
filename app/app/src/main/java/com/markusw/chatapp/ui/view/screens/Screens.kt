3bc055de78e157c2dd48d08b34047a5076cd8d6a
Marco Portacio <marcoportacio4@gmail.com>
2023-01-21 23:21:48 -0500

    Added the necesary dependencies

---
+++ app/app/src/main/java/com/markusw/chatapp/ui/view/screens/Screens.kt
@@ -0,0 +1,6 @@
+package com.markusw.chatapp.ui.view.screens
+
+sealed class Screens(val route: String) {
+    object MainScreen : Screens("main_screen")
+    object ChatScreen : Screens("chat_screen")
+}
---
+++ app/app/src/main/java/com/markusw/chatapp/ui/view/screens/main/MainScreen.kt
@@ -0,0 +1,8 @@
+package com.markusw.chatapp.ui.view.screens.main
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+
+}
