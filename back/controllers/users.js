303714ff53696dbe3edf8a25a0b6b250ff3135c4
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-01-22 01:51:53 -0500

    Creacion del controlador del usuario y rutas

--- back/controllers/users.js
+++ back/controllers/users.js
@@ -1,24 +1,59 @@
-const mongoose = require("mongoose");
 const User = require("../models/user"); //importa tu esquema de Mongoose para usuarios
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ErrorHandler = require("../middleware/errorHandler");
+
+//funcion para obtener todos los usuarios
+exports.getUsers = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find(); //busca todos los usuarios en la base de datos y retorna una promesa
+
+  if (!users) {
+    return next(new ErrorHandler("No hay usuarios", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
 
 //Función para crear un nuevo usuario
-const createUser = (userData) => {
-  const newUser = new User(userData);
-  return newUser.save(); //guarda el nuevo usuario en la base de datos y retorna una promesa
-};
+exports.createUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  if (!user) {
+    return next(new ErrorHandler("Error al crear usuario", 400));
+  }
+
+  res.status(201).json({
+    success: true,
+    user,
+  });
+});
 
 //Función para obtener un usuario por su nombre de usuario
-const getUserByUsername = (username) => {
-  return User.findOne({ username }); //busca el usuario en la base de datos y retorna una promesa
-};
+exports.getUserByUsername = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id); //busca el usuario en la base de datos y retorna una promesa
+
+  if (!user) {
+    return next(new ErrorHandler("Este usuario no existe", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
 
 //Función para eliminar un usuario
-const deleteUser = (username) => {
-  return User.findOneAndDelete({ username }); //elimina el usuario de la base de datos y retorna una promesa
-};
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id); //busca el usuario en la base de datos y retorna una promesa
 
-module.exports = {
-  createUser,
-  getUserByUsername,
-  deleteUser,
-};
+  if (!user) {
+    return next(new ErrorHandler("Error al eliminar usuario", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
--- back/models/user.js
+++ back/models/user.js
@@ -5,6 +5,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    match: /^[a-zA-Z0-9_\-]+$/,
+  },
+  password: {
+    type: String,
+    required: true,
   },
 });
 
